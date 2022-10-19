c11c64df2ab0b9a5ced479573edc5923b9693eb3
Bellatrix-Salungweni1 <bellatrixsalungweni106@student.unsrat.ac.id>
2022-10-19 16:14:46 +0800

    Menambahkan Back to top button

--- js/script.js
+++ js/script.js
@@ -145,3 +145,21 @@ arrowRight.addEventListener("click", function() {
 });
 startSlide();
 
+
+let topButton = document.getElementById("topBtn");
+
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    topButton.style.display = "block";
+  } else {
+    topButton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0; 
+  document.documentElement.scrollTop = 0; 
+}
